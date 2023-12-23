83558e720d2aba1c3fe8ae9adf757d12589eb978
carlos ldeg <carlosldgm@gmail.com>
2023-12-23 13:09:15 -0300

    se crea funcion suma_punto_full que valida si al sumar un punto se gana un game para mostrar como van los games por un tiempo

--- testeoMain.py
+++ testeoMain.py
@@ -19,8 +19,8 @@ def restar_punto(jugador):
     marcador.actualizar_marcador(jugador, -1)
 
 
-def _obtener_points_jugador(jugador,result):
-    #result = _obtener_marcador()
+def _obtener_points_jugador(jugador, result):
+    # result = _obtener_marcador()
     points_jugador = result[jugador]['points']
     # print("puntos jugador " + jugador + " " + str(points_jugador))
     return points_jugador
@@ -104,8 +104,8 @@ def _devuelve_leds_para_enviar_a_matriz_de_un_game(digitos, jugador, set_actual)
 
 def _muestra_points_en_matriz():
     result = _obtener_marcador()
-    points_j1 = _obtener_points_jugador("j1",result)
-    points_j2 = _obtener_points_jugador("j2",result)
+    points_j1 = _obtener_points_jugador("j1", result)
+    points_j2 = _obtener_points_jugador("j2", result)
 
     if points_j1 != "":
         led_iz_point_j1, led_de_point_j1 = _devuelve_leds_para_enviar_a_matriz_de_un_punto(points_j1, "j1")
@@ -123,7 +123,7 @@ def _muestra_games_en_matriz():
     games_j2 = _obtener_games_jugador("j2", result)
     if games_j1 == 0 and games_j2 == 0:
         return
-    set_actual = _obtener_set_actual()  #"1"  # Aca sacar set_actual del json
+    set_actual = _obtener_set_actual()  # "1"  # Aca sacar set_actual del json
     led_iz_game_j1, led_de_game_j1 = _devuelve_leds_para_enviar_a_matriz_de_un_game(games_j1, "j1", set_actual)
     # enciende los leds del digito iz del j1
     sm.pinta_puntos_matriz(led_iz_game_j1, led_de_game_j1)
@@ -132,7 +132,7 @@ def _muestra_games_en_matriz():
     led_iz_game_j2, led_de_game_j2 = _devuelve_leds_para_enviar_a_matriz_de_un_game(games_j2, "j2", set_actual)
     # enciende los leds del digito iz del j2
     sm.pinta_puntos_matriz(led_iz_game_j2, led_de_game_j2)
-    # time.sleep(1)
+    time.sleep(1)
     sm.limpia_matriz()
 
 
@@ -140,18 +140,40 @@ def _limpia_matriz():
     sm.limpia_matriz()
 
 
+def suma_punto_full(jugador):
+    result_prev = _obtener_marcador()
+    games_j1_prev = _obtener_games_jugador(jugador, result_prev)
+    sets_j1_prev = _obtener_set_actual()
+    marcador.actualizar_marcador(jugador) #actualiza punto
+    result_post = _obtener_marcador()
+    games_j1_post = _obtener_games_jugador(jugador, result_post)
+    sets_j1_post = _obtener_set_actual()
+
+    if games_j1_prev != games_j1_post:
+        _muestra_games_en_matriz()
+    else:
+        _muestra_points_en_matriz()
+
+
+def j1_gana_game():
+    # el j1 gana un punto
+    suma_punto_full("j1")  # 15-0
+
+    # el j1 gana un punto
+    suma_punto_full("j1")  # 30-0
+
+    # el j1 gana un punto
+    suma_punto_full("j1")  # 40-0
+
+    # el j1 gana un punto
+    suma_punto_full("j1")  # juego
+
+
 # -----------PRUEBA PARTIDO-------------------
 marcador = lmt.MarcadorTenis()
-#marcador.obtener_marcador()
-
-
 _muestra_points_en_matriz()
 
-sumar_punto("j1")  # 15-0
-_muestra_points_en_matriz()
-
-restar_punto("j1")  # 0-0
-_muestra_points_en_matriz()
+j1_gana_game()
 
 """
 for num_sets in range(2):
